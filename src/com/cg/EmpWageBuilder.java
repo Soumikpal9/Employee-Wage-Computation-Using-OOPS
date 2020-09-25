9e8d11e4fc594074e34643cb63eee95577afabad
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-25 18:20:58 +0530

    Added: Employee Daily Wage Using Switch Case

--- src/com/cg/EmpWageBuilder.java
+++ src/com/cg/EmpWageBuilder.java
@@ -1,6 +1,6 @@
 package com.cg;
 
-//UC3
+//UC4
 public class EmpWageBuilder {
 	//Constants
 	public static final int IS_PART_TIME = 1;
@@ -11,18 +11,19 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		//Variables
 		int empHrs = 0;
-		int empWage =0;
+		int empWage = 0;
 		//Computation
 		for(int i=0;i<NO_OF_EMPLOYEES;i++) {
-			double empcheck = Math.floor(Math.random() * 10) % 2;
-			if(empcheck == IS_FULL_TIME) {
-				empHrs = 8;
-			}
-			else if(empcheck == IS_PART_TIME) {
-				empHrs = 4;
-			}
-			else {
-				empHrs = 0;
+			int empcheck = (int)Math.floor(Math.random() * 10) % 3;
+			switch(empcheck){
+				case IS_PART_TIME:
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+				default:
+					empHrs=0;
 			}
 			empWage = empHrs * EMP_RATE_PER_HOUR;
 			System.out.println("Employee wage : " + empWage);
