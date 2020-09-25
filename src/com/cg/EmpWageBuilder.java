2889e768edf9110a94e7fec8d4ba306782339bab
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-25 18:11:22 +0530

    Added: Employee Attendence

--- src/com/cg/EmpWageBuilder.java
+++ src/com/cg/EmpWageBuilder.java
@@ -1,7 +1,20 @@
 package com.cg;
 
+//UC1
 public class EmpWageBuilder {
+	//Constants
+	public static final int IS_FULL_TIME = 1;
+	public static final int NO_OF_EMPLOYEES = 10;
 	public static void main(String[] args) {
-		System.out.println("hello World !!!");
+		//Computation
+		for(int i=0;i<NO_OF_EMPLOYEES;i++) {
+			double empCheck = Math.floor(Math.random() * 10) % 2;
+			if(empCheck == IS_FULL_TIME) {
+				System.out.println("Employee is present");
+			}
+			else {
+				System.out.println("Employee is absent");
+			}
+		}
 	}
 }
