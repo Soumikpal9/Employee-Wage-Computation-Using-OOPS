85cadfd01c6380fd7d28aa3680fc14bdb606593d
Soumikpal9 <soumikpal1997@gmail.com>
2020-09-27 20:15:25 +0530

    Added: Employee Wage For Multiple Companies Using Array

--- src/com/cg/EmpWageBuilder.java
+++ src/com/cg/EmpWageBuilder.java
@@ -1,6 +1,6 @@
 package com.cg;
 
-public class EmpWageBuilder implements CompEmpWageIF {
+public class EmpWageBuilder {
 	//Constants
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
