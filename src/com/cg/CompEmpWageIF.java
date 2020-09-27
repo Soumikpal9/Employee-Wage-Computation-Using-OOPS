2b9143ac728611735e863cbde27c60488694c6dd
Soumikpal9 <55327053+Soumikpal9@users.noreply.github.com>
2020-09-27 20:16:40 +0530

    Delete CompEmpWageIF.java

--- src/com/cg/CompEmpWageIF.java
+++
@@ -1,7 +0,0 @@
-package com.cg;
-
-public abstract interface CompEmpWageIF {
-	public void AddCompanyEmpWage(String company, int empRatePerHour, int noOfWorkingDays, int maxWorkingHours);
-	public void ComputeEmpWage();
-	public int GetTotalWage(String company);
-}
